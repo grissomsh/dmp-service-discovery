2164ecb14709aa2ea66f7448f3027712001885eb
Grissom <grissom.wang@daocloud.io>
2018-02-28 11:03:10 +0800

    add swagger support

--- src/main/java/io/daocloud/microservice/servicediscovery/ServiceDiscoveryApplication.java
+++ src/main/java/io/daocloud/microservice/servicediscovery/ServiceDiscoveryApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+
 @SpringBootApplication
 @EnableEurekaServer
+@EnableSwagger2Doc
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
